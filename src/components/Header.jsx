13d8611a78829f10090666c9a6301c6a83258b41
Nathaniyell <essien.nathan@yahoo.com>
2023-12-13 16:28:12 +0100

    delete 16 files and update 1 file

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,23 @@
 import React from 'react'
+import Logo from '../assets/logo.png'
 
 const Header = () => {
     const navLinks = ["Home", "Services", "Features", "Contact"]
   return (
-    <nav className='flex items-center justify-between w-10/12 mx-auto py-8 bg-teal-600'>
-        <div>Logo</div>
-        <ul className='flex w-1/3 mx-auto items-center justify-around bg-red-700'>
+    <nav className='flex items-center justify-between w-10/12 mx-auto pt-2 '>
+        <div>
+            <img src={Logo} alt="Logo" />
+        </div>
+        <ul className='flex w-1/3 mx-auto -mr-1 items-center justify-around md:bg-inherit flex-col md:flex-row bg-green-700'>
             {navLinks.map((link, index)=>{
                 return(
-                    <li className='text-lime-950 text-lg cursor-pointer' key={index}>{link}</li>
+                    <li className='text-[#474747] text-lg cursor-pointer' key={index}>{link}</li>
                 )
             })}
         </ul>
-        <aside className='w-1/4 flex justify-around items-center mx-auto bg-blue-500'>
-            <button className='border border-lime-900 bg-teal-800 text-white py-1 px-8 rounded-md'>Login</button>
-            <button className='py-1 px-8 rounded-md border border-teal-800 bg-inherit'>Sign up</button>
+        <aside className='flex gap-8 items-center mx-auto'>
+            <button className='border border-lime-900 bg-teal-800 text-white py-1 px-6 rounded-md'>Login</button>
+            <button className='py-1 px-6 rounded-md border border-teal-800 bg-inherit'>Sign up</button>
         </aside>
     </nav>
   )
