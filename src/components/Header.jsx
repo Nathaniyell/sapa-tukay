935fff99049506b1d8f7d8171aae88e81ea7437f
Nathaniyell <essien.nathan@yahoo.com>
2023-12-13 17:54:23 +0100

    update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -19,14 +19,14 @@ const Header = () => {
       </div>
       <div></div>
       <ul
-        className={`flex w-2/3 gap-2 lg:gap-2 md:w-1/3 mx-auto md:-mr-1 items-center justify-around md:bg-inherit flex-col md:flex-row bg-green-400 ${
+        className={`flex w-2/3 gap-2 lg:gap-2 md:w-1/3 mx-auto md:-mr-1 items-center justify-around md:bg-inherit flex-col md:flex-row bg-teal-600 ${
           navMobile ? "translate-x-0" : "-translate-x-[200%]"
         } transition-transform md:transform-none`}
       >
         {navLinks.map((link, index) => {
           return (
             <li
-              className="text-[#474747] text-xl my-2 hover:underline cursor-pointer"
+              className="text-white md:text-[#474747] text-xl my-2 hover:underline cursor-pointer"
               key={index}
             >
               {link}
@@ -34,7 +34,9 @@ const Header = () => {
           );
         })}
       </ul>
-      <aside className="mt-4 flex flex-col md:flex-row md:gap-8 gap-4 items-center mx-auto">
+      <aside className={`mt-4 flex flex-col md:flex-row md:gap-8 gap-4 items-center mx-auto ${
+          navMobile ? "translate-x-0" : "-translate-x-[250%]"
+        } transition-transform md:transform-none`}>
         <button className="border border-lime-900 bg-teal-800 text-white py-1 px-6 rounded-md">
           Login
         </button>
