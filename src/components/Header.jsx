b21d2b959d731bf44216c8c2114692e2c6b6383d
Nathaniyell <essien.nathan@yahoo.com>
2023-12-13 17:49:13 +0100

    update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,36 +1,49 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import { RxHamburgerMenu } from "react-icons/rx";
 import { GrClose } from "react-icons/gr";
-import Logo from '../assets/logo.png'
+import Logo from "../assets/logo.png";
 
 const Header = () => {
-    const navLinks = ["Home", "Services", "Features", "Contact"]
-    const [navMobile, setNavMobile] = useState(false)
+  const navLinks = ["Home", "Services", "Features", "Contact"];
+  const [navMobile, setNavMobile] = useState(false);
   return (
-    <nav className='flex flex-col md:flex-row items-center justify-between w-11/12 mx-auto pt-2 '>
-        <div className='flex items-center justify-between w-11/12 md:w-fit'>
-            <img src={Logo} alt="Logo" />
-            <button className='text-4xl font-bold text-lime-900 md:invisible' onClick={()=>setNavMobile(!navMobile)}>
-                {navMobile? <GrClose />   :<RxHamburgerMenu /> }
-            </button>
-          
-        </div>
-        <div>
-                 </div>
-                <ul className={`flex w-2/3 gap-2 lg:gap-2 md:w-1/3 mx-auto md:-mr-1 items-center justify-around md:bg-inherit flex-col md:flex-row bg-green-400 ${navMobile?"translate-x-0": "-translate-x-[200%]"} transition-transform md:transform-none`}>
-            {navLinks.map((link, index)=>{
-                return(
-                    <li className='text-[#474747] text-xl my-2 hover:underline cursor-pointer' key={index}>{link}</li>
-                )
-            })}
-        </ul>
-        <aside className='mt-4 flex flex-col md:flex-row md:gap-8 gap-4 items-center mx-auto'>
-            <button className='border border-lime-900 bg-teal-800 text-white py-1 px-6 rounded-md'>Login</button>
-            <button className='py-1 px-6 rounded-md border border-teal-800 bg-inherit'>Sign up</button>
-        </aside>
-        
+    <nav className="flex flex-col md:flex-row items-center justify-between w-11/12 mx-auto pt-2 ">
+      <div className="flex items-center justify-between w-11/12 md:w-fit">
+        <img src={Logo} alt="Logo" />
+        <button
+          className="text-4xl font-bold text-lime-900 md:invisible"
+          onClick={() => setNavMobile(!navMobile)}
+        >
+          {navMobile ? <GrClose /> : <RxHamburgerMenu />}
+        </button>
+      </div>
+      <div></div>
+      <ul
+        className={`flex w-2/3 gap-2 lg:gap-2 md:w-1/3 mx-auto md:-mr-1 items-center justify-around md:bg-inherit flex-col md:flex-row bg-green-400 ${
+          navMobile ? "translate-x-0" : "-translate-x-[200%]"
+        } transition-transform md:transform-none`}
+      >
+        {navLinks.map((link, index) => {
+          return (
+            <li
+              className="text-[#474747] text-xl my-2 hover:underline cursor-pointer"
+              key={index}
+            >
+              {link}
+            </li>
+          );
+        })}
+      </ul>
+      <aside className="mt-4 flex flex-col md:flex-row md:gap-8 gap-4 items-center mx-auto">
+        <button className="border border-lime-900 bg-teal-800 text-white py-1 px-6 rounded-md">
+          Login
+        </button>
+        <button className="py-1 px-6 rounded-md border border-teal-800 bg-inherit">
+          Sign up
+        </button>
+      </aside>
     </nav>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
