510671130734ccdac24c6870784739c4e25f4a5a
Nathaniyell <essien.nathan@yahoo.com>
2023-12-13 15:38:30 +0100

    feat: create Footer.tsx and header.tsx

---
+++ src/components/header.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const header = () => {
+    const navLinks = ["Home", "Services", "Features", "Contact"]
+  return (
+    <nav className='flex items-center justify-between w-10/12 mx-auto'>
+        <div>Logo</div>
+        <ul>
+            {navLinks.map((link, index)=>{
+                return(
+                    <li key={index}>{link}</li>
+                )
+            })}
+        </ul>
+        <aside>
+            <button>Login</button>
+            <button>Sign Up</button>
+        </aside>
+    </nav>
+  )
+}
+
+export default header
