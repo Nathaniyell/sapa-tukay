e86c12158b47988cc5e2dab7aaa342337a65a9e1
Nathaniyell <essien.nathan@yahoo.com>
2023-12-13 16:04:33 +0100

    update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
     const navLinks = ["Home", "Services", "Features", "Contact"]
   return (
-    <nav className='flex items-center justify-between w-10/12 mx-auto py-4'>
+    <nav className='flex items-center justify-between w-10/12 mx-auto py-8'>
         <div>Logo</div>
         <ul className='flex w-2/5 mx-auto items-center justify-around'>
             {navLinks.map((link, index)=>{
@@ -12,9 +12,9 @@ const Header = () => {
                 )
             })}
         </ul>
-        <aside className='w-1/5 flex justify-around items-center'>
-            <button>Login</button>
-            <button>Sign Up</button>
+        <aside className='w-1/4 flex justify-around items-center'>
+            <button className='border border-lime-900 bg-teal-800 text-white py-1 px-8 rounded-md'>Login</button>
+            <button className='py-1 px-8 rounded-md border border-teal-800 bg-inherit'>Sign up</button>
         </aside>
     </nav>
   )
