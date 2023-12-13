15ede01fb17e93c381b7407a7e63c7d98e0013f0
Nathaniyell <essien.nathan@yahoo.com>
2023-12-13 15:58:39 +0100

    update App.js and Header.jsx

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="bg-lime-300 min-h-screen">
+    <div className="bg-lime-200 min-h-screen">
       <Header />
     </div>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,16 +3,16 @@ import React from 'react'
 const Header = () => {
     const navLinks = ["Home", "Services", "Features", "Contact"]
   return (
-    <nav className='flex items-center justify-between w-10/12 mx-auto'>
+    <nav className='flex items-center justify-between w-10/12 mx-auto py-4'>
         <div>Logo</div>
-        <ul>
+        <ul className='flex w-2/5 mx-auto items-center justify-around'>
             {navLinks.map((link, index)=>{
                 return(
-                    <li key={index}>{link}</li>
+                    <li className='text-lime-950 text-lg' key={index}>{link}</li>
                 )
             })}
         </ul>
-        <aside>
+        <aside className='w-1/5 flex justify-around items-center'>
             <button>Login</button>
             <button>Sign Up</button>
         </aside>
