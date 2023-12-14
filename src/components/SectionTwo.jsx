ed0cf578abbd5d05a18e35a7013bf456eda493ef
Nathaniyell <essien.nathan@yahoo.com>
2023-12-14 07:45:56 +0100

    update SectionTwo.jsx

--- src/components/SectionTwo.jsx
+++ src/components/SectionTwo.jsx
@@ -1,14 +1,46 @@
-import React from 'react'
+import React from "react";
+import lady from "../assets/lady_sapa.png";
+import greetBros from "../assets/greet_bros.png";
+import guy from "../assets/guy_sapa.png";
 
 function SectionTwo() {
+  const cards = [
+    {
+      title: "No more Sapa",
+      img: guy,
+    },
+    {
+      title: "Beg",
+      img: lady,
+    },
+    {
+      title: "Send money",
+      img: greetBros,
+    },
+  ];
   return (
     <>
-    <div className='text-[#474747] text-center'>
-        <h1 className='text-xl'>What you stand to benefit</h1>
-        <p>This is how our product works</p>
-    </div>
+      <div className="text-[#474747] text-center font-medium">
+        <h1 className="text-3xl leading-loose">What you stand to benefit</h1>
+        <p className="text-xl">This is how our product works</p>
+      </div>
+      <div>
+        {cards.map((card) => {
+          return (
+            <div>
+              <h5 className="text-center text-[#474747] text-2xl font-medium">
+                {card.title}
+              </h5>
+              <p>
+                In Nigerian Pidgin English, Sapa is sometimes used to describe
+                financial hardship or running out of money.
+              </p>
+            </div>
+          );
+        })}
+      </div>
     </>
-  )
+  );
 }
 
-export default SectionTwo
+export default SectionTwo;
