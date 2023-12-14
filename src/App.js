752ec3a160cffcc98223140a60475982275a654c
Nathaniyell <essien.nathan@yahoo.com>
2023-12-14 08:04:04 +0100

    update App.js, Hero.jsx and SectionTwo.jsx

--- src/App.js
+++ src/App.js
@@ -7,10 +7,12 @@ import SectionTwo from "./components/SectionTwo";
 
 function App() {
   return (
-    <div className="bg-[#ebfbb3] h-[150vh]">
+    <div className="bg-[#ebfbb3] pb-10">
       <Header />
       <Hero />
+      <div className="relative -top-64 md:-top-0">
       <SectionTwo />
+      </div>
     </div>
   );
 }
--- src/components/SectionTwo.jsx
+++ src/components/SectionTwo.jsx
@@ -24,12 +24,12 @@ function SectionTwo() {
         <h1 className="text-3xl leading-loose">What you stand to benefit</h1>
         <p className="text-xl">This is how our product works</p>
       </div>
-      <div className="flex flex-col md:flex-row justify-between items-center">
+      <div className="flex flex-col md:flex-row gap-6 justify-between items-center w-11/12 mx-auto my-14">
         {cards.map((card) => {
           return (
             <div key={card.title} className="bg-white p-4 rounded-md">
-                <img src={card.img} alt={card.title} />
-              <h5 className="text-center text-[#474747] text-2xl font-medium">
+                <img src={card.img} alt={card.title} className="mx-auto" />
+              <h5 className="leading-loose text-center text-[#474747] text-2xl font-medium">
                 {card.title}
               </h5>
               <p>
