e6307ea7d1d88fd738177757e02a5fc63a704c60
Nathaniyell <essien.nathan@yahoo.com>
2023-12-15 19:44:40 +0100

    update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
   return (
     <nav className="flex flex-col md:flex-row items-center justify-between w-11/12 mx-auto pt-2 z-10 bg-red-400">
       <div className="flex items-center justify-between w-full md:w-fit">
-        <img src={Logo} alt="Logo" />
+        <img src={Logo} className="md:mr-20 lg:mr-0" alt="Logo" />
         <button
           className="text-4xl font-bold cursor-pointer text-lime-900 md:hidden"
           onClick={() => setNavMobile(!navMobile)}
@@ -18,21 +18,21 @@ const Header = () => {
         </button>
       </div>
             <ul
-        className={`bg-[#ebfbb3] z-10 flex w-3/5 mx-auto gap-2 lg:gap-2 items-center justify-around md:bg-inherit flex-col md:flex-row  ${
+        className={`bg-[#ebfbb3] z-10 flex w-full lg:w-1/4 mx-auto gap-2 lg:gap-2 items-center justify-around md:bg-inherit flex-col md:flex-row  ${
           navMobile ? "translate-x-0" : "-translate-x-[200%]"
         } transition-transform md:transform-none border border-teal-800 md:border-none`}
       >
         {navLinks.map((link, index) => {
           return (
             <li
-              className="text-[#474747] text-xl my-2 hover:underline cursor-pointer"
+              className="text-[#474747] text-xl my-2 lg:mx-4 hover:underline cursor-pointer"
               key={index}
             >
               {link}
             </li>
           );
         })}
-      <aside className={`z-10 mt-4 md:mt-0 md:ml-8 flex flex-col md:flex-row gap-4 items-center mx-auto pb-4 md:pb-0`}>
+      <aside className={`z-10 mt-4 md:mt-0 md:ml-8 lg:ml-20 flex flex-col md:flex-row gap-4 items-center mx-auto pb-4 md:pb-0`}>
         <button className="border border-lime-900 bg-teal-800 text-white py-1 px-6 rounded-md">
           Login
         </button>
