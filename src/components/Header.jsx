d31225eb9fbccbd9ccfce1dcf195b8da4048d4f6
Nathaniyell <essien.nathan@yahoo.com>
2023-12-15 19:11:39 +0100

    update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,19 +7,18 @@ const Header = () => {
   const navLinks = ["Home", "Services", "Features", "Contact"];
   const [navMobile, setNavMobile] = useState(false);
   return (
-    <nav className="flex flex-col md:flex-row items-center justify-between w-11/12 mx-auto pt-2 z-10">
+    <nav className="flex flex-col md:flex-row items-center justify-between w-11/12 mx-auto pt-2 z-10 bg-red-600 ">
       <div className="flex items-center justify-between w-11/12 md:w-fit">
         <img src={Logo} alt="Logo" />
         <button
-          className="text-4xl font-bold cursor-pointer text-lime-900 md:invisible"
+          className="text-4xl font-bold cursor-pointer text-lime-900 md:hidden"
           onClick={() => setNavMobile(!navMobile)}
         >
           {navMobile ? <GrClose /> : <RxHamburgerMenu />}
         </button>
       </div>
-      <div></div>
-      <ul
-        className={`z-10 flex w-2/3 gap-2 lg:gap-2 md:w-1/3 mx-auto md:-mr-1 items-center justify-around md:bg-inherit flex-col md:flex-row bg-teal-600 ${
+            <ul
+        className={`bg-yellow-500 z-10 flex w-2/3 gap-2 lg:gap-2 mx-auto md: items-center justify-around md:bg-inherit flex-col md:flex-row  ${
           navMobile ? "translate-x-0" : "-translate-x-[200%]"
         } transition-transform md:transform-none`}
       >
@@ -33,17 +32,15 @@ const Header = () => {
             </li>
           );
         })}
-      </ul>
-      <aside className={`z-10 mt-4 flex flex-col md:flex-row md:gap-6 gap-4 items-center mx-auto ${
-          navMobile ? "translate-x-0" : "-translate-x-[300%]"
-        } transition-transform md:transform-none`}>
+      <aside className={`z-10 mt-4 md:mt-0 md:ml-8 flex flex-col md:flex-row gap-4 items-center mx-auto w-full`}>
         <button className="border border-lime-900 bg-teal-800 text-white py-1 px-6 rounded-md">
           Login
         </button>
-        <button className="py-1 px-6 rounded-md border border-teal-800 bg-inherit">
+        <button className="py-1 w-20 rounded-md border border-teal-800 bg-inherit">
           Sign up
         </button>
       </aside>
+      </ul>
     </nav>
   );
 };
