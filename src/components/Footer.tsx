ee3549ba7eeb4e55c373416c59aa03a7f76b25bf
Nathaniyell <essien.nathan@yahoo.com>
2023-12-16 19:33:49 +0100

    update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import footerImg from "../assets/footer_img.png"
 
 const Footer = () => {
   return (
-    <div className='bg-[#00545F]'>Footer</div>
+    <div className='bg-[#00545F] flex items-center gap-6 flex-col md:flex-row p-4 md:gap-10'>
+        <img src={footerImg} alt='footer'/>
+        </div>
   )
 }
 
