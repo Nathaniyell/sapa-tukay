928d84855ef2d84bb1443c5dda4f040cef5611f7
Nathaniyell <essien.nathan@yahoo.com>
2023-12-16 19:42:07 +0100

    update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,6 +4,9 @@ import footerImg from "../assets/footer_img.png"
 const Footer = () => {
     const whatWeDo = ["What we do", "Features", "Blog"]
     const whoWeAre = ["About us","Careers","Brand Center","Privacy"]
+    const needHelp = ["Contact Us","Help Center","Download","Security Advisories"]
+
+
   return (
     <div className='bg-[#00545F] flex items-center gap-6 flex-col md:flex-row p-4 md:gap-10'>
         <img src={footerImg} alt='footer'/>
