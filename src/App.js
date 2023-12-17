16e8cc10c04ff3d2fcf3dc03f5d1422893eb975b
Nathaniyell <essien.nathan@yahoo.com>
2023-12-17 22:53:57 +0100

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import Footer from "./components/Footer";
+
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import SectionTwo from "./components/SectionTwo";
@@ -16,7 +16,7 @@ function App() {
       <div className="relative top-28">
          <SectionTwo />
          </div>
-         <Footer />
+         {/* <Footer /> */}
        </div>
   );
 }
