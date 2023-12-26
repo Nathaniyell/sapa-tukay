98c108ded516303872b604cc889c71be87703416
Nathaniyell <essien.nathan@yahoo.com>
2023-12-27 00:54:53 +0100

    update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import Settings from "../assets/settings.png"
 import HeroImg from "../assets/herologo.png"
+import { motion } from 'framer-motion'
 
 const Hero = () => {
   return (
     <div className='relative top-20 flex items-center justify-between flex-col-reverse md:flex-row z-0'>
-        <div className='w-10/12 mx-auto md:w-1/2 text-[#474747]'>
+        <motion.div
+              initial={{x: -200, opacity: 0}}
+              whileInView={{opacity: 1, x:0}}
+              transition={{duration:.7, delay: 0.4}}
+              viewport={{once: true}}
+        className='w-10/12 mx-auto md:w-1/2 text-[#474747]'>
             <h1 className='text-[40px] md:text-6xl font-semibold leading-relaxed'>Get your urgent 2k</h1>
             <p className='text-xl font-medium'>No cash, No wahala. Get our application and deal with Sapa</p>
           <div className='md:w-2/5 flex justify-between items-center md:gap-4 my-2'>
@@ -22,8 +28,13 @@ const Hero = () => {
             <button className='border border-lime-900 bg-teal-800 text-white px-4 py-2 rounded-md'>Get started</button>
             <span className='hover:underline flex items-center font-light'><img className='inline' src={Settings} alt="Settings" />&nbsp;How it works</span>
           </span>
-        </div>
-    <img className='w-10/12 md:w-[40%] mx-auto relative overflow-x-hidden' src={HeroImg} alt="Hero" />
+        </motion.div>
+    <motion.img
+     initial={{x: 200, opacity: 0}}
+     whileInView={{opacity: 1, x:0}}
+     transition={{duration:.7, delay: 0.4}}
+     viewport={{once: true}}
+    className='w-10/12 md:w-[40%] mx-auto relative overflow-x-hidden' src={HeroImg} alt="Hero" />
     </div>
   )
 }
