c58131e70a7dee74d624b06788d0da0605327035
Nathaniyell <essien.nathan@yahoo.com>
2023-12-26 23:34:10 +0100

    update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,6 +10,16 @@ const Footer = () => {
   return (
     <div className='bg-[#00545F] relative mt-48 bottom-0 flex items-center gap-6 flex-col md:flex-row p-4 md:gap-10'>
         <img src={footerImg} className='w-1/5' alt='footer'/>
+        <div>
+          <h1>What we do</h1>
+          <ul>
+            {
+              whatWeDo.map((item, index)=>{
+               return  <li key={index}>{item}</li>
+              })
+            }
+          </ul>
+        </div>
         </div>
   )
 }
